e6d8ef220c0fc122cb7b1ba002ee29f378f76a81
arslansajid <arslansajid951@gmail.com>
2019-12-10 11:38:26 +0500

    data file

---
+++ src/data.js
@@ -0,0 +1,242 @@
+export const giftItems = [
+  {
+    id: 1,
+    name: "Stocking fillers",
+    // desc: "zip-front padded jacket",
+    // price: "$788",
+    image: "product1.gif",
+  },
+  {
+    id: 2,
+    name: "Gifts to impress",
+    // desc: "zip-front padded jacket",
+    // price: "$788",
+    image: "product4.gif",
+  },
+  {
+    id: 3,
+    name: "Everyone loves",
+    // desc: "zip-front padded jacket",
+    // price: "$788",
+    image: "product3.gif",
+  },
+];
+
+export const partyShoesItems = [
+  {
+    id: 1,
+    name: "Emmanuel Olunkwa",
+    desc: "Wears Grenson x Craig Green",
+    // price: "$788",
+    image: "waffle_stack.jpg",
+  },
+  {
+    id: 2,
+    name: "Joshua Rowley",
+    desc: "Wears Gucci",
+    // price: "$788",
+    image: "waffle_stack2.jpg",
+  },
+  {
+    id: 3,
+    name: "A.CHAL",
+    desc: "Wears Giuseppe Zanotti",
+    // price: "$788",
+    image: "waffle_stack3.jpg",
+  },
+];
+
+export const newItems = [
+  {
+    id: 3,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack3.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/65/76/11/14657611_22900968_480.jpg",
+  },
+  {
+    id: 2,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/51/80/41/14518041_22896796_480.jpg",
+  },
+  {
+    id: 3,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack2.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/65/76/11/14657611_22900968_480.jpg",
+  },
+  {
+    id: 4,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack4.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/51/80/41/14518041_22896796_480.jpg",
+  },
+];
+
+export const watchItems = [
+  {
+    id: 1,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/23/52/42/14235242_22532928_480.jpg",
+  },
+  {
+    id: 2,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack2.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/41/94/38/14419438_21374032_480.jpg",
+  },
+  {
+    id: 3,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack3.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/14/61/09/14146109_22532760_480.jpg",
+  },
+  {
+    id: 4,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack4.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/01/17/63/14011763_18218433_480.jpg",
+  },
+];
+
+export const shoesItems = [
+  {
+    id: 1,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack4.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/23/52/42/14235242_22532928_480.jpg",
+  },
+  {
+    id: 2,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack3.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/41/94/38/14419438_21374032_480.jpg",
+  },
+  {
+    id: 3,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack2.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/14/61/09/14146109_22532760_480.jpg",
+  },
+  {
+    id: 4,
+    name: "Marcelo Burlon County Of Milan",
+    desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/01/17/63/14011763_18218433_480.jpg",
+  },
+];
+
+
+export const reviews = [
+  {
+    id: 1,
+    name: 'Arslan Sajid',
+    comment: "Everyone wanted to know where we got it. It was yellow cake with cannoli filling and it was to die for. The cake was SO moist and the cannoli filling was amazing.I will definitely be ordering from Dream Cakes again! Thank you!! “"
+  },
+  {
+    id: 2,
+    name: 'Imran Khan',
+    comment: "This classic cheesecake gets a sweet twist with the pineapple topping and can be enjoyed on a special occasion.Prepare using apricot or peach preserves.Then, once the cake was cut and everybody started eating, people could not stop talking about how delicious it was!"
+  },
+  {
+    id: 3,
+    name: 'Wasim Akram',
+    comment: "Everyone wanted to know where we got it. It was yellow cake with cannoli filling and it was to die for. The cake was SO moist and the cannoli filling was amazing.I will definitely be ordering from Dream Cakes again! Thank you!! “"
+  },
+  {
+    id: 4,
+    name: 'Shoaib Malik',
+    comment: "This classic cheesecake gets a sweet twist with the pineapple topping and can be enjoyed on a special occasion.Prepare using apricot or peach preserves.Then, once the cake was cut and everybody started eating, people could not stop talking about how delicious it was!"
+  },
+]
+
+export const Videos = [
+  {
+    id: 1,
+    title: 'Waffles',
+    url: 'https://youtu.be/lpTej1o5Uu4'
+  },
+  {
+    id: 2,
+    title: 'Crepes Pancakes',
+    url: 'https://youtu.be/lpTej1o5Uu4'
+  },
+  {
+    id: 3,
+    title: 'Gelato & Pastry',
+    url: 'https://youtu.be/lpTej1o5Uu4'
+  },
+  {
+    id: 4,
+    title: 'Toppings & Decorations',
+    url: 'https://youtu.be/lpTej1o5Uu4'
+  },
+]
+
+export const categories = [
+  {
+    id: 1,
+    name: "Waffles, Crepes Pancakes",
+    // desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack3.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/65/76/11/14657611_22900968_480.jpg",
+  },
+  {
+    id: 2,
+    name: "Gelato & Pastry",
+    // desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/51/80/41/14518041_22896796_480.jpg",
+  },
+  {
+    id: 3,
+    name: "Toppings & Decorations",
+    // desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack2.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/65/76/11/14657611_22900968_480.jpg",
+  },
+  {
+    id: 4,
+    name: "Brands",
+    // desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack4.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/51/80/41/14518041_22896796_480.jpg",
+  },
+  {
+    id: 5,
+    name: "Equipments",
+    // desc: "zip-front padded jacket",
+    price: "$788",
+    image: "waffle_stack.jpg",
+    hoverImage: "https://cdn-images.farfetch-contents.com/14/51/80/41/14518041_22896796_480.jpg",
+  },
+];
