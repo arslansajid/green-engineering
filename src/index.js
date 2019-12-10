de6035f1665c832257a6560835b6565067559bb4
arslansajid <arslansajid951@gmail.com>
2019-12-10 11:34:11 +0500

    index.js file fixed

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { HashRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import "react-image-gallery/styles/css/image-gallery.css";
+import { createBrowserHistory } from 'history';
+import configureStore from './store/ConfigureStore';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+const history = createBrowserHistory({ basename: '/' });
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>
+    ,document.getElementById('root'));
+  
+  
+  serviceWorker.unregister();
