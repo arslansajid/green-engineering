d8f1891dbdf928f3536bf503510744a8921851a2
arslansajid <arslansajid951@gmail.com>
2019-12-10 11:36:52 +0500

    Added contact us page

---
+++ src/containers/ContactUs/ContactUs.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './ContactUs.css';
+
+export default class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        
+    }
+  }
+
+  render() {
+    return (
+      <div className="about-us-page">
+        <div className="container my-3">
+          <h3>ContactUs</h3>
+        </div>
+      </div>
+    )
+  }
+}
