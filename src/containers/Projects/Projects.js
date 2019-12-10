f79860e011e8850e23a801e04af7b416030c4ccb
arslansajid <arslansajid951@gmail.com>
2019-12-10 11:37:37 +0500

    Added projects page

---
+++ src/containers/Projects/Projects.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './Projects.css';
+
+export default class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        
+    }
+  }
+
+  render() {
+    return (
+      <div className="about-us-page">
+        <div className="container my-3">
+          <h3>Projects</h3>
+        </div>
+      </div>
+    )
+  }
+}
