22bc727bb35be8aab61d7bb861a06f4abebdfb56
arslansajid <arslansajid951@gmail.com>
2019-12-13 09:50:30 +0500

    building page added

--- src/containers/Operations/BuildingRetexture.js
+++ src/containers/Operations/BuildingRetexture.js
@@ -11,11 +11,36 @@ export default class AboutUs extends Component {
 
   render() {
     return (
-      <div className="about-us-page">
-        <div className="container my-3">
-          <h3>BuildingRetexture</h3>
+      <div className="building-retexture">
+        <div className="flyer-section" />
+          <div className="container">
+            <div className="row">
+              <div className="col">
+              <div className="px-5">
+                <h3 className="heading my-3">
+                  {'Building & Retexture'}
+                </h3>
+
+                <hr />
+
+                <p className="my-3">
+                  Engineering Design Bureauhas forged  a  strong  team  of   experts  comprising  of  architects  and  structural  engineers.  The  firm  has  handled  several  projects  in  this  field, which  include,  Commercial,  Industrial  and  Residential  Building  and  other  Civil  Engineering  Projects.
+                </p>
+
+                <p>
+                  The firm also offers integrated services covering Architecture, Structural Engineering, Sanitation Works, Water works, Electrification and Air-Conditioning Multistory Buildings and Industrial Buildings are a  specialty  of  the  firm.  The  firm  has  affected  great  saving  for   its clients through proper planning, adoption of correct structural systems and careful handling  of  detailed  design  using  sophisticated  design  procedures.  The   firm  has  been  responsible  for  the  design  of  overhead  Water tanks, Multistoried Office Blocks, Factory Building, Warehouses, Blocks of Residential Flats and other Building Complex.
+                </p>
+
+                <p className="mb-5">
+                  The firm has substantial individual experience in the design of heavy foundation subjected to vibratory loads.
+                </p>
+                
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
   }
 }
+
