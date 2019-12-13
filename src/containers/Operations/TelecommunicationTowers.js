8729a560f617faf93cf5b564e17c1f189c91daf7
arslansajid <arslansajid951@gmail.com>
2019-12-13 09:51:12 +0500

    telecom towers page added

--- src/containers/Operations/TelecommunicationTowers.js
+++ src/containers/Operations/TelecommunicationTowers.js
@@ -11,9 +11,36 @@ export default class AboutUs extends Component {
 
   render() {
     return (
-      <div className="about-us-page">
-        <div className="container my-3">
-          <h3>TelecommunicationTowers</h3>
+      <div className="telecommunication-towers">
+        <div className="flyer-section" />
+          <div className="container">
+            <div className="row">
+              <div className="col">
+              <div className="px-5">
+                <h3 className="heading my-3">
+                  {'Telecommunication / Microwave Towers'}
+                </h3>
+
+                <hr />
+
+                <p className="my-3">
+                  Engineering Design Bureau is one of the leading firms in the design of Microwave Towers. The firm has designed and supervised over 8000 towers, both self-supported and guyed masts, for various clients. This field of engineering is the mainstay of company. All our designers are carried out with the most sophisticated structural analysis programs and are in accordance with the latest engineering practices. The services offered in this field are:
+                </p>
+
+                <ol>
+                  <li>Detailed Design of Structure and Foundations</li>
+                  <li>Detailed Drawings</li>
+                  <li>Bill of Quantities</li>
+                  <li>Tender Documents</li>
+                  <li>Fabrication Details</li>
+                  <li>Fabrication Supervision</li>
+                  <li>Construction/Erection supervision</li>
+                  <li>Inspection Report</li>
+                </ol>
+
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
