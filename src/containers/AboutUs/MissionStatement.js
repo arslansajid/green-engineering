672ead63dd14e802ecdbd56c396667805cc80058
arslansajid <arslansajid951@gmail.com>
2019-12-13 09:49:42 +0500

    statement page added

--- src/containers/AboutUs/MissionStatement.js
+++ src/containers/AboutUs/MissionStatement.js
@@ -11,9 +11,26 @@ export default class AboutUs extends Component {
 
   render() {
     return (
-      <div className="about-us-page">
-        <div className="container my-3">
-          <h3>MissionStatement</h3>
+      <div className="mission-statement">
+        <div className="flyer-section" />
+
+          <div className="container">
+            <div className="row">
+              <div className="col">
+                <div className="px-5">
+                  <h3 className="heading my-3">
+                    Mission Statement
+                  </h3>
+
+                  <hr />
+
+                  <p className="my-3 text-center">
+                    "Together we strive to achieve our Vision to Transform Pakistan through Persistent Hard Work, Superior Work Ethic,
+                    Due Diligence and Professional Excellence."
+                  </p>
+                </div>
+            </div>
+          </div>
         </div>
       </div>
     )
