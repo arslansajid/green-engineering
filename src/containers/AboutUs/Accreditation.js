2377adfc66124910daf8f7aadcf6761a2ce58e12
arslansajid <arslansajid951@gmail.com>
2019-12-13 09:48:53 +0500

    accrediation page added

--- src/containers/AboutUs/Accreditation.js
+++ src/containers/AboutUs/Accreditation.js
@@ -11,9 +11,28 @@ export default class AboutUs extends Component {
 
   render() {
     return (
-      <div className="about-us-page">
-        <div className="container my-3">
-          <h3>Accreditation</h3>
+      <div className="accreditation">
+        <div className="flyer-section" />
+          <div className="container">
+            <div className="row">
+              <div className="col">
+              <div className="px-5">
+                <h3 className="heading my-3">
+                  Accreditation
+                </h3>
+
+                <hr />
+
+                <p className="my-3">
+                At EDB we ensure to implement most advance systems accepted at local and global level. In acquisition of these systems, we are training our staff according to the requirement of new challenges,  policies & vision  and  made  necessary  changes  in  our  documentation and working procedures. Our accreditation with  local and international bodies are showing our interest to approach the right path.
+                </p>
+
+                <p>
+                Certification from different local institutions i.e. Pakistan Engineering Council defines that we meet defined local and worldwide parameters and going step further each time.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
